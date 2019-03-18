92c11be126824ba17ecf8dffc39c28cbb1d21596
Khoa-N <35976559+Khoa-N@users.noreply.github.com>
2019-03-17 17:20:59 -0700

    first

--- src/Board.js
+++ src/Board.js
@@ -20,59 +20,76 @@ export default class Board extends React.Component {
       inProgress: React.createRef(),
       complete: React.createRef(),
     }
+    this.drake = Dragula([], {});
   }
   getClients() {
     return [
-      ['1','Stark, White and Abbott','Cloned Optimal Architecture', 'in-progress'],
-      ['2','Wiza LLC','Exclusive Bandwidth-Monitored Implementation', 'complete'],
-      ['3','Nolan LLC','Vision-Oriented 4Thgeneration Graphicaluserinterface', 'backlog'],
-      ['4','Thompson PLC','Streamlined Regional Knowledgeuser', 'in-progress'],
-      ['5','Walker-Williamson','Team-Oriented 6Thgeneration Matrix', 'in-progress'],
-      ['6','Boehm and Sons','Automated Systematic Paradigm', 'backlog'],
-      ['7','Runolfsson, Hegmann and Block','Integrated Transitional Strategy', 'backlog'],
-      ['8','Schumm-Labadie','Operative Heuristic Challenge', 'backlog'],
-      ['9','Kohler Group','Re-Contextualized Multi-Tasking Attitude', 'backlog'],
-      ['10','Romaguera Inc','Managed Foreground Toolset', 'backlog'],
-      ['11','Reilly-King','Future-Proofed Interactive Toolset', 'complete'],
-      ['12','Emard, Champlin and Runolfsdottir','Devolved Needs-Based Capability', 'backlog'],
-      ['13','Fritsch, Cronin and Wolff','Open-Source 3Rdgeneration Website', 'complete'],
-      ['14','Borer LLC','Profit-Focused Incremental Orchestration', 'backlog'],
-      ['15','Emmerich-Ankunding','User-Centric Stable Extranet', 'in-progress'],
-      ['16','Willms-Abbott','Progressive Bandwidth-Monitored Access', 'in-progress'],
-      ['17','Brekke PLC','Intuitive User-Facing Customerloyalty', 'complete'],
-      ['18','Bins, Toy and Klocko','Integrated Assymetric Software', 'backlog'],
-      ['19','Hodkiewicz-Hayes','Programmable Systematic Securedline', 'backlog'],
-      ['20','Murphy, Lang and Ferry','Organized Explicit Access', 'backlog'],
+      ['1', 'Stark, White and Abbott', 'Cloned Optimal Architecture', 'in-progress'],
+      ['2', 'Wiza LLC', 'Exclusive Bandwidth-Monitored Implementation', 'complete'],
+      ['3', 'Nolan LLC', 'Vision-Oriented 4Thgeneration Graphicaluserinterface', 'backlog'],
+      ['4', 'Thompson PLC', 'Streamlined Regional Knowledgeuser', 'in-progress'],
+      ['5', 'Walker-Williamson', 'Team-Oriented 6Thgeneration Matrix', 'in-progress'],
+      ['6', 'Boehm and Sons', 'Automated Systematic Paradigm', 'backlog'],
+      ['7', 'Runolfsson, Hegmann and Block', 'Integrated Transitional Strategy', 'backlog'],
+      ['8', 'Schumm-Labadie', 'Operative Heuristic Challenge', 'backlog'],
+      ['9', 'Kohler Group', 'Re-Contextualized Multi-Tasking Attitude', 'backlog'],
+      ['10', 'Romaguera Inc', 'Managed Foreground Toolset', 'backlog'],
+      ['11', 'Reilly-King', 'Future-Proofed Interactive Toolset', 'complete'],
+      ['12', 'Emard, Champlin and Runolfsdottir', 'Devolved Needs-Based Capability', 'backlog'],
+      ['13', 'Fritsch, Cronin and Wolff', 'Open-Source 3Rdgeneration Website', 'complete'],
+      ['14', 'Borer LLC', 'Profit-Focused Incremental Orchestration', 'backlog'],
+      ['15', 'Emmerich-Ankunding', 'User-Centric Stable Extranet', 'in-progress'],
+      ['16', 'Willms-Abbott', 'Progressive Bandwidth-Monitored Access', 'in-progress'],
+      ['17', 'Brekke PLC', 'Intuitive User-Facing Customerloyalty', 'complete'],
+      ['18', 'Bins, Toy and Klocko', 'Integrated Assymetric Software', 'backlog'],
+      ['19', 'Hodkiewicz-Hayes', 'Programmable Systematic Securedline', 'backlog'],
+      ['20', 'Murphy, Lang and Ferry', 'Organized Explicit Access', 'backlog'],
     ].map(companyDetails => ({
       id: companyDetails[0],
       name: companyDetails[1],
       description: companyDetails[2],
-      status: companyDetails[3],
+      status: 'backlog',
     }));
   }
-  renderSwimlane(name, clients, ref) {
+  renderSwimlane(name, clients, value, ref) {
     return (
-      <Swimlane name={name} clients={clients} dragulaRef={ref}/>
+      <Swimlane name={name} clients={clients} dragulaRef={ref} value={value} />
     );
   }
-
   render() {
+    console.log(this.swimlanes.backlog.current);
     return (
       <div className="Board">
         <div className="container-fluid">
-          <div className="row">
+          <div className="row" >
             <div className="col-md-4">
-              {this.renderSwimlane('Backlog', this.state.clients.backlog, this.swimlanes.backlog)}
+              {this.renderSwimlane('Backlog', this.state.clients.backlog, this.swimlanes.backlog, this.dragulaReference)}
             </div>
             <div className="col-md-4">
-              {this.renderSwimlane('In Progress', this.state.clients.inProgress, this.swimlanes.inProgress)}
+              {this.renderSwimlane('In Progress', this.state.clients.inProgress, this.swimlanes.inProgress, this.dragulaReference)}
             </div>
             <div className="col-md-4">
-              {this.renderSwimlane('Complete', this.state.clients.complete, this.swimlanes.complete)}
+              {this.renderSwimlane('Complete', this.state.clients.complete, this.swimlanes.complete, this.dragulaReference)}
             </div>
           </div>
         </div>
       </div>
     );
+  }
+  changeStatus(e, status) {
+    if (status === "Backlog") {
+      e.status = this.swimlanes.backlog;
+      e.className = 'Card Card-grey';
+    } else if (status === "In Progress") {
+      e.status = this.swimlanes.inProgress;
+      e.className = 'Card Card-blue';
+    } else {
+      e.status = this.swimlanes.complete;
+      e.className = 'Card Card-green';
+    }
+  }
+  dragulaReference = (componentBackingInstance) => {
+    this.drake.containers.push(componentBackingInstance);
+    this.drake.on("drop", (el, target, source, sibling) => this.changeStatus(el, target.previousSibling.innerHTML));
   }
 }
